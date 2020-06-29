13bb86f804d74c3c10a7f908b0a3406af60908d3
Emiliano Carreño <emiliano0662@gmail.com>
2020-06-29 11:17:31 -0500

    ajustes generales

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
 import './scss/style.scss';
 import 'bootstrap';
 
-import { WOW } from 'wowjs';
 import { Swiper, Pagination, Navigation, Thumbs } from 'swiper/js/swiper.esm.js';
-
-/*Se inicializa el plugin para las animaciones*/
-var wow = new WOW({
-    live: false,
-    scrollContainer: null
-});
 
 /*Todos los "load" cargan los bloques recurrentes*/
 $('header').load("header.html");
@@ -82,5 +75,37 @@ if ($.contains(document.body, document.getElementById('swiper-services-thumbs'))
     });
 }
 
-/*Se agregan las animaciones para toda la pagina que no cargan de menera recurrente*/
-wow.init();
+$('header').on('click', '.btn-scrollTop', function(event) {
+    event.preventDefault();
+
+    var item = $(this).data('item');
+
+    $('body,html').animate({ scrollTop: $(item).offset().top - 50 }, 3000, 'swing');
+});
+
+$(document).on('submit', '#form-contact', function(event) {
+    event.preventDefault();
+
+    $('#form-contact .btn-primary').prop('disabled', true);
+
+    $.ajax({
+        cache: false,
+        type: $(this).attr("method"),
+        url: $(this).attr("action"),
+        data: $(this).serialize(),
+        success: function(data) {
+
+            $('.alert').hide();
+            $('#form-contact .btn-primary').prop('disabled', false);
+
+            if (data) {
+                $('.alert-success').fadeIn();
+                $("#form-contact")[0].reset();
+            } else {
+                $('.alert-danger').fadeIn();
+            }
+
+            setTimeout(function() { $('.alert').hide(); }, 5000);
+        }
+    });
+});
