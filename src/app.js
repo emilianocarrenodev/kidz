5be60ff847ba661aab15729af58314e518e458b9
Emiliano Carreño <emiliano0662@gmail.com>
2020-06-29 00:45:34 -0500

    avances

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import './scss/style.scss';
 import 'bootstrap';
 
 import { WOW } from 'wowjs';
-import { Swiper } from 'swiper/js/swiper.esm.js';
+import { Swiper, Pagination, Navigation, Thumbs } from 'swiper/js/swiper.esm.js';
 
 /*Se inicializa el plugin para las animaciones*/
 var wow = new WOW({
@@ -17,11 +17,68 @@ $('footer').load("footer.html");
 /*Script para el slider del home*/
 if ($.contains(document.body, document.getElementById('slider-main'))) {
 
+    Swiper.use([Pagination]);
+
     var mySwiper = new Swiper('#slider-main', {
+        loop: true,
         autoplay: {
             delay: 5000,
         },
-        loop: true
+        pagination: {
+            el: '.swiper-pagination',
+            type: "bullets",
+            clickable: true,
+        }
+    });
+}
+
+/*Script slider quienes somos*/
+if ($.contains(document.body, document.getElementById('swiper-gallery-thumbs'))) {
+
+    Swiper.use([Navigation, Thumbs]);
+
+    var galleryThumbs = new Swiper('#swiper-gallery-thumbs', {
+        spaceBetween: 15,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+
+    var galleryTop = new Swiper('#swiper-gallery-main', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.btn-slider-about-us.btn-next',
+            prevEl: '.btn-slider-about-us.btn-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+    });
+}
+
+/*Script slider interno servicios*/
+if ($.contains(document.body, document.getElementById('swiper-services-thumbs'))) {
+
+    Swiper.use([Navigation, Thumbs]);
+
+    var galleryThumbs = new Swiper('#swiper-services-thumbs', {
+        spaceBetween: 15,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+
+    var galleryTop = new Swiper('#swiper-services-main', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.btn-slider-services.btn-next',
+            prevEl: '.btn-slider-services.btn-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
     });
 }
 
